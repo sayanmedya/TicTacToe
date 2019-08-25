969e2f1ffed8e5e09fbd263e7c219f2c711f55aa
Sayan Medya <45382595+sayanmedya@users.noreply.github.com>
2019-08-25 23:41:57 +0530

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,19 +1,9 @@
+#ifndef TICTACTOE_DEFINITION
+#define TICTACTOE_DEFINITION
+
 #include <iostream>
-
-class tictactoe {
-private:
-    std::string player1, player2;
-    char grid[3][3];
-
-public:
-    tictactoe();
-    void get_player_names();
-    std::string player_1();
-    std::string player_2();
-    void print_grid();
-    void input(char);
-    bool if_won();
-};
+#include "tictactoe.h"
+using namespace std;
 
 tictactoe::tictactoe()
 {
@@ -22,45 +12,27 @@ tictactoe::tictactoe()
             grid[i][j] = '-';
 }
 
-void tictactoe::get_player_names()
-{
-    std::cout << "Enter Player 1 name: ";
-    getline(std::cin, player1);
-    std::cout << "Enter Player 2 name: ";
-    getline(std::cin, player2);
-}
-
-std::string tictactoe::player_1()
-{
-    return player1;
-}
-
-std::string tictactoe::player_2()
-{
-    return player2;
-}
-
 void tictactoe::print_grid()
 {
     for (int i = 0;i < 3;i++) {
         for (int j = 0;j < 3;j++)
             if (grid[i][j] == 'X')
-                std::cout << "\033[1m\033[31mX \033[0m";
+                cout << "\033[1m\033[31mX \033[0m";
             else if (grid[i][j] == 'O')
-                std::cout << "\033[1m\033[34mO \033[0m";
+                cout << "\033[1m\033[34mO \033[0m";
             else
-                std::cout << grid[i][j] << " ";
-        std::cout << "\n";
+                cout << grid[i][j] << " ";
+        cout << "\n";
     }
 }
 
 void tictactoe::input(char c)
 {
     int x, y;
-    std::cin >> x >> y;
+    cin >> x >> y;
     while (grid[x - 1][y - 1] != '-' || x < 1 || x > 3 || y < 1 || y > 3) {
-        std::cout << "Wrong input. Try again: ";
-        std::cin >> x >> y;
+        cout << "Wrong input. Try again: ";
+        cin >> x >> y;
     }
     grid[x - 1][y - 1] = c;
 }
@@ -79,3 +51,5 @@ bool tictactoe::if_won()
         return 1;
     return 0;
 }
+
+#endif
