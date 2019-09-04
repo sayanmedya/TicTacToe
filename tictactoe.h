2a622924d2ce3c8c6d3ee14ee1a789a5c243bed3
Sayan Medya <45382595+sayanmedya@users.noreply.github.com>
2019-09-05 02:03:57 +0530

    Update tictactoe.h

--- tictactoe.h
+++ tictactoe.h
@@ -1,15 +1,20 @@
-#ifndef TICTACTOE_DECLARATION
-#define TICTACTOE_DECLARATION
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+
+#include <iostream>
+#include "getch.h"
+
+void sleepcp(int milliseconds);
 
 class tictactoe {
 protected:
     char grid[3][3];
-
 public:
     tictactoe();
     void print_grid();
-    void input(char);
+    char input(char);
     bool if_won();
+    void do_last_move(char c);
 };
 
 #endif
